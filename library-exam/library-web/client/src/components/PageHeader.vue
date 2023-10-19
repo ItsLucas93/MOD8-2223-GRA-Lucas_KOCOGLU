8aecaa998a9e2a46b433c41a84ca80cf7bf3135c
Lucas <45604254+ItsLucas93@users.noreply.github.com>
2023-10-19 16:32:07 -0400

    6. PAGE HEADER COMPONENT

--- library-exam/library-web/client/src/components/PageHeader.vue
+++ library-exam/library-web/client/src/components/PageHeader.vue
@@ -9,6 +9,7 @@ const route = useRoute()
 const router = useRouter()
 const authenticationService = useAuthenticationService()
 const searchText = ref(route.query.search || '')
+const user = authenticationService.user
 
 router.afterEach((to, from) => {
   if (to.name === home) {
@@ -58,16 +59,16 @@ function collapseWhitespace(string) {
           <li>
             <div><RouterLink :to="{ name: 'about' }">About</RouterLink></div>
           </li>
-          <li>
+          <li v-if="user && user.role === 'librarian'">
             <div><RouterLink :to="{ name: 'add' }">Add book</RouterLink></div>
           </li>
-          <li>
+          <li v-if="!user">
             <div><RouterLink :to="{ name: 'signup' }">Sign up</RouterLink></div>
           </li>
-          <li>
+          <li v-if="!user">
             <div><RouterLink :to="{ name: 'login' }">Log in</RouterLink></div>
           </li>
-          <li>
+          <li v-if="user">
             <div><a @click="authenticationService.logOut">Log out</a></div>
           </li>
         </ul>
